f9c8c63d393d03a70da0af372bda2ec72b59dd85
Cheks <okekechekwube1@gmail.com>
2020-12-22 08:25:53 +0100

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const user = mongoose.Schema({
     name: { type: String, unique: true },
     points: { type: Number, default: 0 },
-}, { timestamps: true });
+});
 
 module.exports = mongoose.model('User', user);
